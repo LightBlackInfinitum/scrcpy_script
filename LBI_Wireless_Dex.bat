1b5adb51a7f5892b825781cac985a690ab801ad1
Blakenidby <63127265+Blakenidby@users.noreply.github.com>
2021-05-26 20:29:30 -0300

    Update LBI_Wireless_Dex.bat

--- LBI_Wireless_Dex.bat
+++ LBI_Wireless_Dex.bat
@@ -1,41 +1,58 @@
 @echo off
-echo "STARTING ANDROID DESKTOP (SCRIPT MADE BY LIGHT BLACK INFINITUM) CHECK OUR GROUP: https://t.me/AndroidDesktop"
+
+REM ADB Connecting
 cd "c:\adb\"
 adb kill-server
 adb start-server
 adb -s DEVICE_ID tcpip 5555
 adb devices
 adb connect DEVICE_IP:5555
-echo "=================================STARTING DESKTOP MODE======================================"
-adb -s DEVICE_IP:5555 shell settings put system accelerometer_rotation 0 
-adb -s DEVICE_IP:5555 shell settings put system user_rotation 3
-echo "=================================LOADING DESKTOP SCREEN MODE================================"
-adb -s DEVICE_IP:5555 shell cmd package set-home-activity com.farmerbb.taskbar/.activity.HomeActivity
-echo "=================================CONVERTING TO DESKTOP STYLE================================"
-adb -s DEVICE_IP:5555 shell wm density 215
-echo "=================================DELETING ON SCREEN KEYBOARD================================"
-adb -s DEVICE_IP:5555 shell ime set com.wparam.nullkeyboard/.NullKeyboard
-echo "=================================INICIATING DESKTOP MODE AND GOING TO HOME SCREEN==========="
+
+REM Device Information (optional)
+adb -s DEVICE_IP:5555 shell wm density
+adb -s DEVICE_IP:5555 shell wm size 
+adb -s DEVICE_IP:5555 shell getprop ro.product.model
+adb -s DEVICE_IP:5555 shell getprop ro.build.version.release
+adb -s DEVICE_IP:5555 shell getprop ro.product.board
+adb -s DEVICE_IP:5555 shell dumpsys battery
+
+REM Screen
 adb -s DEVICE_IP:5555 shell settings put system screen_off_timeout 2147483647
-adb -s DEVICE_IP:5555 shell input keyevent 3
-adb -s DEVICE_IP:5555 shell input keyevent 3
+adb -s DEVICE_IP:5555 shell wm size 1920x1080
+adb -s DEVICE_IP:5555 shell wm density 215
+
+REM Launcher
+adb -s DEVICE_IP:5555 shell settings put global policy_control immersive.full=*
+adb -s DEVICE_IP:5555 shell cmd package set-home-activity com.farmerbb.taskbar/.activity.HomeActivity
+adb -s DEVICE_IP:5555 shell am start -W -c android.intent.category.HOME -a android.intent.action.MAIN
+
+REM Rotation
+adb -s DEVICE_IP:5555 shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
+
+REM Keyboard
+adb -s DEVICE_IP:5555 shell ime set com.wparam.nullkeyboard/.NullKeyboard
 
 
-scrcpy -s DEVICE_IP:5555 --render-driver=opengl --rotation 0 -m1280 -b4M -f -w --window-title 'INFINITUM-DESKTOP'
+REM Scrcpy
+scrcpy --serial DEVICE_IP:5555 --render-driver=direct3d -m1366 -b6M --max-fps 30 -f -Sw --disable-screensaver --window-title 'ANDROID-DESKTOP'
 
 
-echo "=================================EXITING DESKTOP MODE======================================="
-echo "=================================RESTORING ON SCREEN KEYBOARD==============================="
-adb -s DEVICE_IP:5555 shell ime set com.google.android.inputmethod.latin/com.android.inputmethod.latin.LatinIME
-echo "=================================LOADING PHONE MOBILE SCREEN==============================="
-adb -s DEVICE_IP:5555 shell cmd package set-home-activity bitpit.launcher/.ui.HomeActivity
-adb -s DEVICE_IP:5555 shell settings put system accelerometer_rotation 1
-adb -s DEVICE_IP:5555 shell settings put system user_rotation 1
-echo "=================================CONVERTING TO PHONE STYLE================================="
-adb -s DEVICE_IP:5555 shell wm density 360
-echo "=================================GOING TO HOME SCREEN======================================"
+REM Screen
 adb -s DEVICE_IP:5555 shell settings put system screen_off_timeout 60000
-adb -s DEVICE_IP:5555 shell input keyevent 3
-adb -s DEVICE_IP:5555 shell input keyevent 3
-echo "QUIT SUCESS, PRESS ANY KEY TO CLOSE THIS WINDOW. REMEMBER VISIT US: https://t.me/AndroidDesktop"
+adb -s DEVICE_IP:5555 shell wm size reset
+adb -s DEVICE_IP:5555 shell wm density reset
+
+REM Rotation
+adb -s DEVICE_IP:5555 shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:1
+
+REM Keyboard
+adb -s DEVICE_IP:5555 shell ime set com.google.android.inputmethod.latin/com.android.inputmethod.latin.LatinIME
+
+REM Launcher
+adb -s DEVICE_IP:5555 shell cmd package set-home-activity bitpit.launcher/.ui.HomeActivity
+adb -s DEVICE_IP:5555 shell am force-stop com.farmerbb.taskbar
+adb -s DEVICE_IP:5555 shell settings put global policy_control null*
+adb -s DEVICE_IP:5555 shell am start -W -c android.intent.category.HOME -a android.intent.action.MAIN
+
+echo "REMEMBER VISIT US: https://t.me/joinchat/SLlAIfdCFxLeexVB"
 pause
