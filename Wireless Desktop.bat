e2e6efd0cb210be5dab0b508779c576770c980db
Blakenidby <63127265+Blakenidby@users.noreply.github.com>
2021-05-31 20:13:20 -0300

    Update Wireless Desktop.bat

--- Wireless Desktop.bat
+++ Wireless Desktop.bat
@@ -10,12 +10,12 @@ adb -s DEVICE_ID tcpip 5555
 adb connect DEVICE_IP:5555
 
 REM Device Information (optional)
-adb -s DEVICE_IP:5555 shell wm density
-adb -s DEVICE_IP:5555 shell wm size 
-adb -s DEVICE_IP:5555 shell getprop ro.product.model
-adb -s DEVICE_IP:5555 shell getprop ro.build.version.release
-adb -s DEVICE_IP:5555 shell getprop ro.product.board
-adb -s DEVICE_IP:5555 shell dumpsys battery
+REM adb -s DEVICE_IP:5555 shell wm density
+REM adb -s DEVICE_IP:5555 shell wm size 
+REM adb -s DEVICE_IP:5555 shell getprop ro.product.model
+REM adb -s DEVICE_IP:5555 shell getprop ro.build.version.release
+REM adb -s DEVICE_IP:5555 shell getprop ro.product.board
+REM adb -s DEVICE_IP:5555 shell dumpsys battery
 
 REM Screen
 adb -s DEVICE_IP:5555 shell settings put system screen_off_timeout 2147483647
