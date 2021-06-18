4dfb867324c47c2a8fd406b9f7cb6cb24e454f66
Blakenidby <63127265+Blakenidby@users.noreply.github.com>
2021-06-18 19:49:30 -0300

    Update USB Desktop.bat

--- USB Desktop.bat
+++ USB Desktop.bat
@@ -14,10 +14,12 @@ REM adb -s DEVICE_ID shell dumpsys battery
 REM Screen
 adb -s DEVICE_ID shell wm size 1920x1080
 adb -s DEVICE_ID shell wm density 215
-adb -s DEVICE_ID shell settings put system font_scale  1.15
+adb -s DEVICE_ID shell settings put system font_scale  1.20
 
 REM Launcher
 adb -s DEVICE_ID shell settings put global policy_control immersive.full=*
+adb -s DEVICE_ID shell pm enable com.farmerbb.taskbar
+adb -s DEVICE_ID shell pm disable-user bitpit.launcher
 adb -s DEVICE_ID shell cmd package set-home-activity com.farmerbb.taskbar/.activity.HomeActivity
 adb -s DEVICE_ID shell am start -W -c android.intent.category.HOME -a android.intent.action.MAIN
 
@@ -32,16 +34,17 @@ adb -s DEVICE_ID shell cmd vibrator vibrate 100
 adb -s DEVICE_ID shell cmd vibrator vibrate 100
 adb -s DEVICE_ID shell cmd vibrator vibrate 100
 adb -s DEVICE_ID shell cmd vibrator vibrate 100
-
+timeout /t 1 /nobreak >nul
 
 REM Scrcpy
-scrcpy -s DEVICE_ID --render-driver=direct3d --rotation 0 -m1366 -b80M --max-fps 30 -f -Sw --disable-screensaver --window-title 'ANDROID-DESKTOP'
+scrcpy -s DEVICE_ID --render-driver=direct3d --rotation 0 -m1366 -b60M --max-fps 30 -f -Sw --disable-screensaver --window-title 'ANDROID-DESKTOP'
 
 
 REM Screen
 adb -s DEVICE_ID shell wm size reset
 adb -s DEVICE_ID shell wm density reset
 adb -s DEVICE_ID shell settings put system font_scale  1.0
+timeout /t 1 /nobreak >nul
 
 REM Rotation
 adb -s DEVICE_ID shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:1
@@ -50,12 +53,14 @@ REM Keyboard
 adb -s DEVICE_ID shell ime set com.google.android.inputmethod.latin/com.android.inputmethod.latin.LatinIME
 
 REM Launcher
-adb -s DEVICE_ID shell cmd package set-home-activity bitpit.launcher/.ui.HomeActivity
 adb -s DEVICE_ID shell settings put global policy_control null*
+adb -s DEVICE_ID shell pm enable bitpit.launcher
+adb -s DEVICE_ID shell pm disable-user com.farmerbb.taskbar
+adb -s DEVICE_ID shell cmd package set-home-activity bitpit.launcher/.ui.HomeActivity
 adb -s DEVICE_ID shell am start -W -c android.intent.category.HOME -a android.intent.action.MAIN
 
 REM Stuff
 adb -s DEVICE_ID shell cmd vibrator vibrate 100
 
 echo "REMEMBER VISIT US: https://t.me/joinchat/SLlAIfdCFxLeexVB"
-pause
+timeout /t 10 /nobreak >nul
